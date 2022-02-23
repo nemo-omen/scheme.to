6f4ede1ae3c0e8d5782b6d41d9f0992354d239e0
trainingmontage <nemo.omen@gmail.com>
2022-02-23 11:40:36 -0600

    Mess around with the router some more.

--- scripts/router.js
+++ scripts/router.js
@@ -18,19 +18,24 @@ const routes = [
  ];
  
  // okay, let's try the 'revealing module pattern'
- // with this little router
+ // with this little router.
  // @see: https://coryrylan.com/blog/javascript-module-pattern-basics
  const router = (function () {
    let outlet = document.querySelector('main');
  
-   // converts href string into a URL object
-   // and returns URL.pathname
+   // convert href string into a URL object and return URL.pathname
+   // so we can avoid getting mired in regex (winning)
    const getPath = function (href) {
      return new URL(href).pathname;
    };
 
+   // this function will split the given path into an array of path segments
+   // we can use this to match our defined routes in order to provide
+   // the page templates we want to the DOM
    const matchRoute = function (path) {
-      console.log('matching!');
+      console.log('matching: ', path);
+      const segments = path.split('/').slice(1);
+      console.log('segments: ', segments);
       // naive! -- make this match longer paths!
       // @see: https://www.willtaylor.blog/client-side-routing-in-vanilla-js/
       return routes.find((route) => route.path === path);
@@ -47,14 +52,30 @@ const routes = [
    };
  })();
  
- // route listener
+// route listener
+//  I'm naming and exporting this as an IIFE so that: 
+// a) I can keep this logic in a separate file
+// b) It executes, then attaches the listener as soon as the import loads
+// Note: This isn't something I had to look up, it just seemed to make
+// sense. Before learning more about IIFEs, I would have had to import
+// this and call it explicitly. I feel like this will be useful in the future.
+
  export const routeListener = (function () {
+   //  listen for all window click events
    window.addEventListener('click', (event) => {
-     if (!event.target.matches('[data-route]')) return;
-     // prevent links from navigating away from the page
-     event.preventDefault();
- 
-     router.loadPage(event.target.href);
+      // if they have a specific data-attribute, it
+      // means we should modify their default behavior
+      // and use our router to deliver dynamic content
+      // rather than navigating to a new location and losing
+      // our aplication state
+
+      if (!event.target.matches('[data-route]')) return;
+      
+      // prevent links from navigating away from the page
+      event.preventDefault();
+      
+      // send the target element's href to the router   
+      router.loadPage(event.target.href);
    });
  })();
  
