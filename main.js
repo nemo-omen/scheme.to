034cdc3d43c5d7b17cd2b2da25aea6dc0167707f
trainingmontage <nemo.omen@gmail.com>
2022-03-05 22:22:02 -0600

    Messing with curried fns

--- main.js
+++ main.js
@@ -2,28 +2,33 @@ const createElement = tagName => document.createElement(tagName);
 
 const text = element => text => element.innerText = text;
 
+const child = element => childElement => {
+   element.appendChild(childElement);
+};
+
+const attributes = element => attributes =>  
+   attributes.forEach(
+      attribute => {
+         element.setAttribute(Object.keys(attribute)[0], Object.values(attribute)[0]);
+      });
+
 const children = element => childElements => {
-   childElements.forEach((child) => {
-      element.appendChild(child);
+   childElements.forEach((childElement) => {
+      if(Array.from(childElement.childNodes).length < 1) {
+         child(element)(childElement);
+      }
    });
 };
 
-const child = element => child => {
-   console.log({child});
-   element.appendChild(child);
-}
+const compose = (...fns) => x =>
+   fns.reduceRight((acc, fn) => fn(acc), x);
 
 const app = document.getElementById('app');
 
-const p = createElement('p');
-
-text(p)('This is a paragraph!');
-
 const div = createElement('div');
 
-
-child(app)(div);
-
+attributes(div)([{class: 'okay'}]);
+const p = createElement('p');
+text(p)('I am a paragraph');
 child(div)(p);
-
-console.log(p);
+child(app)(div);
