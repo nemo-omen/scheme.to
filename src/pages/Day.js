eeaba6968583b8763a9b70b36dd096b99cedc3bc
trainingmontage <nemo.omen@gmail.com>
2022-03-11 11:30:42 -0600

    Start project planner preliminary layout exploration

--- src/pages/Day.js
+++ src/pages/Day.js
@@ -5,10 +5,37 @@ const Day = (function () {
    return function (outlet) {
       
       const template = `
-         <h2>Day</h2>
-         <div class="hero">
-            <h3>Agenda</h3>
-         </div>
+         <div class="page planner-page" id="planner">
+            <div class="planner-header">
+               <div class="planner-header-section planner-meta">
+                  <fieldset>
+                     <label for="project-name">Project:</label>
+                     <input type="text" id="project-name"/>
+                  </fieldset>
+                  <fieldset>
+                     <label for="project-start-date">Start Date:</label>
+                     <input type="text" id="project-start-date"/>
+                  </fieldset>
+                  <fieldset>
+                     <label for="project-name">Due Date:</label>
+                     <input type="text" id="project-due-date"/>
+                  </fieldset>
+                  <fieldset>
+                     <label for="project-name">Status:</label>
+                     <select id="project-name">
+                        <option value="">Set Status</option>
+                        <option value="planning">Planning</option>
+                        <option value="working">Working</option>
+                        <option value="waiting">Waiting For</option>
+                        <option value="completed">Completed</option>
+                        <option value="abandoned">Abandoned</option>
+                     </input>
+                  </fieldset>
+               </div>
+               <div class="planer-header-section">
+                  
+               </div>
+            </div>
       `;
 
       // mount the above template to the parent node
