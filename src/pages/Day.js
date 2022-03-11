1a0c416e5300b3ca7299bd90c2bda0e17469e739
trainingmontage <nemo.omen@gmail.com>
2022-03-11 17:27:24 -0600

    Start creating project planner inputs

--- src/pages/Day.js
+++ src/pages/Day.js
@@ -7,35 +7,43 @@ const Day = (function () {
       const template = `
          <div class="page planner-page" id="planner">
             <div class="planner-header">
+
                <div class="planner-header-section planner-meta">
                   <fieldset>
-                     <label for="project-name">Project:</label>
-                     <input type="text" id="project-name"/>
+                     <legend id="name-label">Project:</legend>
+                     <input type="text" id="name-input" aria-labelledby="name-label"/>
                   </fieldset>
                   <fieldset>
-                     <label for="project-start-date">Start Date:</label>
-                     <input type="text" id="project-start-date"/>
+                     <legend id="start-date-label">Start Date:</legend>
+                     <input type="text" id="start-date-input" aria-labelledby="start-date-label"/>
                   </fieldset>
                   <fieldset>
-                     <label for="project-name">Due Date:</label>
-                     <input type="text" id="project-due-date"/>
+                     <legend id="due-date-label">Due Date:</legend>
+                     <input type="text" id="due-date-input" aria-labelledby="due-date-label"/>
                   </fieldset>
                   <fieldset>
-                     <label for="project-name">Status:</label>
-                     <select id="project-name">
+                     <legend id="status-input-label">Status:</legend>
+                     <select id="status-input" aria-labelledby="status-input-label">
                         <option value="">Set Status</option>
                         <option value="planning">Planning</option>
                         <option value="working">Working</option>
                         <option value="waiting">Waiting For</option>
                         <option value="completed">Completed</option>
                         <option value="abandoned">Abandoned</option>
-                     </input>
+                     </select>
                   </fieldset>
                </div>
-               <div class="planer-header-section">
-                  
+
+               <div class="planner-header-section">
+                  <fieldset>
+                     <legend id="goals-label">Goals:</legend>
+                     <input type="text" id="goal-1-input"/>
+                     <button>+</button>
+                  </fieldset>
                </div>
+
             </div>
+         </div>
       `;
 
       // mount the above template to the parent node
