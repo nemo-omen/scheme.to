b5884d1408726fd7caf51681e5a9b8bdfe045972
nemo-omen <nemo.omen@gmail.com>
2022-03-13 08:48:24 -0500

    Remove base router init

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 import {select} from './util/domUtils.js';
-import routeListener from './lib/router.js';
-import {store} from './lib/Store.js';
+// import routeListener from './lib/router.js';
+// import {store} from './lib/Store.js';
 
 const menuButton = select('.menu-toggle');
 const menu = select('nav');
 
-routeListener.listen();
+// routeListener.listen();
 
 menuButton.addEventListener('click', function () {
    menu.classList.toggle('menu-open');
 });
 
 // listen for custom 'pageloaded' event
-window.addEventListener('pageloaded', function (event) {
-   console.log('loadedPage: ', event.detail);
-   store.set(
-      'currentPage', 
-      event.detail
-   );
-});
+// window.addEventListener('pageloaded', function (event) {
+//    console.log('loadedPage: ', event.detail);
+//    store.set(
+//       'currentPage', 
+//       event.detail
+//    );
+// });
